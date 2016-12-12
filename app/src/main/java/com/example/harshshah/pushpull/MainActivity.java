0da686b1e43ca8e8cd6ceb54bf56245e7d923fab
Harsh <harsh.hbs@gmail.com>
2016-12-11 19:38:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/harshshah/pushpull/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.harshshah.pushpull;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void harshV1(){
+        String s1 = "hi 1";
+    }
+
+
+}
